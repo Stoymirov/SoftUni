7e9c797abdbdf2d7b3ad42511b551565e256d7e5
Stoymirov <dimitarstoymirov0@gmail.com>
2023-10-01 01:32:37 +0300

    03.ExerciseFinished

--- Fundamentals/03.Exercise/06/Program.cs
+++ Fundamentals/03.Exercise/06/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] arr = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+bool isFound = false;
+int foundnumber = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    
+    int sumOfj = 0;
+    int sumOfk = 0;
+    for (int j = 0; j < i ; j++)
+    {
+        sumOfj += arr[j];
+    }
+    for (int k = i+1; k < arr.Length; k++)
+    {
+        sumOfk += arr[k];
+    }
+
+    if (sumOfj == sumOfk)
+    {
+        isFound = true;
+        foundnumber = i;
+    }
+    
+}
+if (isFound)
+{
+    Console.WriteLine(foundnumber);
+}
+else Console.WriteLine("no");
---
+++ Fundamentals/03.Exercise/08/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] arr = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int n = int.Parse( Console.ReadLine() );
+int controlNumber = 0;
+while (true)
+{
+    for (int i = 0 + controlNumber; i < arr.Length; i++)
+    {
+        if (controlNumber == i)
+        {
+            continue; 
+        }
+        if (arr[controlNumber] + arr[i] == n)
+        {
+            Console.WriteLine(arr[controlNumber] + " " + arr[i]);
+        }
+
+        
+    }
+    controlNumber++;
+    if (controlNumber == arr.Length)
+    {
+        break;
+    }
+}
