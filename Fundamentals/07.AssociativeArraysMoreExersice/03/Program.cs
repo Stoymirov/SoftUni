ca2fa539f923f47d83edfa7c003cc8e02000e730
Stoymirov <dimitarstoymirov0@gmail.com>
2023-11-07 23:11:11 +0200

    more exersice

---
+++ Fundamentals/07.AssociativeArraysMoreExersice/03/Program.cs
@@ -0,0 +1,90 @@
+﻿
+using System.Text.RegularExpressions;
+
+Dictionary<string, List<Position>> myDictionary = new Dictionary<string, List<Position>>();
+
+string command = String.Empty;
+
+while ((command = Console.ReadLine()) != "Season end")
+{
+    string[] array = Regex.Split(command, @" -> | vs ");
+
+    if (array.Length == 3)
+    {
+        string player = array[0];
+        string position = array[1];
+        int skill = int.Parse(array[2]);
+
+        var contains = myDictionary.ContainsKey(player);
+        if (!contains)
+        {
+            myDictionary.Add(player, new List<Position>());
+        }
+
+        var existiongPosition = myDictionary[player].Find(x => x.Name == position);
+        if (existiongPosition == null)
+        {
+            myDictionary[player].Add(new Position(position, skill));
+        }
+        else
+        {
+            if (existiongPosition.Skill < skill)
+            {
+                existiongPosition.Skill = skill;
+            }
+        }
+    }
+    else
+    {
+        string player1 = array[0];
+        string player2 = array[1];
+        if (myDictionary.ContainsKey(player2) && myDictionary.ContainsKey(player1))
+        {
+            var vaarr = myDictionary[player1].Any(x => myDictionary[player2].Any(y => x.Name == y.Name));
+
+            if (vaarr)
+            {
+                int player1Score = myDictionary[player1].Sum(x => x.Skill);
+                int player2Score = myDictionary[player2].Sum(x => x.Skill);
+
+                if (player1Score > player2Score)
+                {
+                    myDictionary.Remove(player2);
+
+                }
+                else if (player2Score > player1Score)
+                {
+                    myDictionary.Remove(player1);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
+
+foreach (var VARIABLE in myDictionary.OrderByDescending(x => x.Value.Sum(x => x.Skill)).ThenBy(x => x.Key))
+{
+    Console.WriteLine($"{VARIABLE.Key}: {VARIABLE.Value.Sum(x => x.Skill)} skill");
+    foreach (var VARIABLE2 in VARIABLE.Value.OrderByDescending(x => x.Skill).ThenBy(x => x.Name))
+    {
+        Console.WriteLine($"- {VARIABLE2.Name} <::> {VARIABLE2.Skill}");
+    }
+}
+
+
+
+
+
+class Position
+{
+    public Position(string name, int skill)
+    {
+        Name = name;
+        Skill = skill;
+    }
+    public string Name { get; set; }
+    public int Skill { get; set; }
+}
---
+++ Fundamentals/07.AssociativeArraysMoreExersice/04/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+
+Dictionary<string, Dictionary<string, Dwarf>> myDictionary = new Dictionary<string, Dictionary<string,Dwarf>>();
+    
+
+string command = "";
+while ((command = Console.ReadLine()) != "Once upon a time")
+{
+    string[] array = command.Split(" <:> ");
+    string name = array[0];
+    string color = array[1];
+    int physics = int.Parse(array[2]);
+
+    if (!myDictionary.ContainsKey(color))
+    {
+        myDictionary.Add(color, new Dictionary<string, Dwarf>());
+    }
+
+    if (myDictionary[color].ContainsKey(name))
+    {
+        if (myDictionary[color][name].Physics < physics)
+        {
+            myDictionary[color][name].Physics = physics;
+        }
+    }
+    else
+    {
+        Dwarf newDwarf = new Dwarf(name, physics);
+        myDictionary[color].Add(name,newDwarf);
+    }
+}
+
+foreach(var VARIABLE in myDictionary.OrderByDescending(x => x.Value.Count))
+{
+    foreach (var VARIABLE2 in VARIABLE.Value.Values.OrderByDescending(x => x.Physics).ThenBy(x => VARIABLE.Value.Count))
+    {
+        Console.WriteLine($"({VARIABLE.Key}) {VARIABLE2.Name} <-> {VARIABLE2.Physics}");
+    }
+}
+
+
+
+class Dwarf
+{
+    public Dwarf(string name,int physics)
+    {
+        Name = name;
+        Physics = physics;
+       
+    }
+    public string Name { get; set; }
+    public int Physics { get; set; }
+  
+}
